6b799611c86d1600ab3769ead8713383079eee4c
Desmond Wong <dweh29@yahoo.com>
2022-03-26 10:39:00 +0800

    Javascript file + Images

---
+++ scripts/Main.js
@@ -0,0 +1,233 @@
+//to print current day and date
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const monthnames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var now = new Date();
+let day = weekday[now.getDay()];
+let month = monthnames[now.getMonth()];
+var year = now.getFullYear();
+var date = now.getDate();
+document.getElementById("daydate").innerHTML = day + ", " + date + " " + month + " " + year;
+
+// COVID 19 STATS & CHARTS
+// SELECT ALL ELEMENTS
+const country_name_element = document.querySelector(".country .country-name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+// APP VARIABLES
+let app_data = [],
+  cases_list = [],
+  recovered_list = [],
+  deaths_list = [],
+  dates = [],
+  formatedDates = [];
+    
+// SET MALAYSIA COUNTRY CODE
+let country_code = "MY";
+let user_country = "Malaysia";
+fetchData(user_country);
+
+// FETCH API
+function fetchData(country) {
+  user_country = country;
+  country_name_element.innerHTML = "Loading...";
+
+  (cases_list = []),
+    (recovered_list = []),
+    (deaths_list = []),
+    (dates = []),
+    (formatedDates = []);
+  
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/confirmed",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          dates.push(entry.Date);
+          cases_list.push(entry.Cases);
+        });
+      });
+    
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/recovered",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          recovered_list.push(entry.Cases);
+        });
+      });
+    
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/deaths",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          deaths_list.push(entry.Cases);
+        });
+      });
+    
+    updateUI();
+  };
+
+  api_fetch(country);
+}
+
+// UPDATE UI
+function updateUI() {
+  updateStats();
+  axesLinearChart();
+}
+
+function updateStats() {
+  const total_cases = cases_list[cases_list.length - 1];
+  const new_confirmed_cases = total_cases - cases_list[cases_list.length - 2];
+
+  const total_recovered = recovered_list[recovered_list.length - 1];
+  const new_recovered_cases = total_recovered - recovered_list[recovered_list.length - 2];
+
+  const total_deaths = deaths_list[deaths_list.length - 1];
+  const new_deaths_cases = total_deaths - deaths_list[deaths_list.length - 2];
+
+  country_name_element.innerHTML = user_country;
+  total_cases_element.innerHTML = total_cases;
+  new_cases_element.innerHTML = `+${new_confirmed_cases}`;
+  recovered_element.innerHTML = total_recovered;
+  new_recovered_element.innerHTML = `+${new_recovered_cases}`;
+  deaths_element.innerHTML = total_deaths;
+  new_deaths_element.innerHTML = `+${new_deaths_cases}`;
+
+  // FORMAT DATES
+  dates.forEach((date) => {
+    formatedDates.push(formatDate(date));
+  });
+}
+
+// UPDATE CHART
+let my_chart;
+function axesLinearChart() {
+  if (my_chart) {
+    my_chart.destroy();
+  }
+
+  my_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: cases_list,
+          fill: false,
+          borderColor: "#FFF",
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+        },
+        {
+          label: "Recovered",
+          data: recovered_list,
+          fill: false,
+          borderColor: "#009688",
+          backgroundColor: "#009688",
+          borderWidth: 1,
+        },
+        {
+          label: "Deaths",
+          data: deaths_list,
+          fill: false,
+          borderColor: "#f44336",
+          backgroundColor: "#f44336",
+          borderWidth: 1,
+        },
+      ],
+      labels: formatedDates,
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+}
+
+// FORMATE DATES
+function formatDate(dateString) {
+  let date = new Date(dateString);
+  return `${date.getDate()} ${monthnames[date.getMonth()]}`;
+}
+
+// SLIDES
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+var slideTitle = document.querySelectorAll(".slide-title");
+var i;
+for (i = 0; i < slideTitle.length; i++) {
+  slideTitle[i].innerHTML = "10 Things you can do to manage your COVID-19 symptoms at home";
+}
+
+// FIT TEXT INTO CONTAINER
+$('#authorFitText').textfill({
+  maxFontPixels: 50,
+  minFontPixels: 25
+});
+
+$('#quoteFitText').textfill({
+  maxFontPixels: 50,
+  minFontPixels: 25,
+});
+
+// RETURN TO TOP 
+function topFunction() {
+  $('html, body').animate({scrollTop : 0},800);
+} 
+
+
+
